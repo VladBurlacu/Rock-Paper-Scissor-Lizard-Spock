a631e0d009e5d2a85c26b4cb223f495d4ca85404
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-20 11:46:00 +0200

    Remade the entire javascript to make it simpler. Not happy with the games state but will come back to it later

--- mainJavaScript.js
+++ mainJavaScript.js
@@ -1,52 +1,68 @@
-const userChoiceDisplay = document.createElement(`h1`);
-const computerChoiceDisplay = document.createElement(`h1`);
-const resultDisplay = document.createElement(`h1`);
-const gameGrid = document.getElementById(`game`);
-gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay);
-
-const choices = ["rock", "paper", "scissors", "lizards", "spock"];
-let userChoice;
+let playerChoice;
 let computerChoice;
 
-function generateComputerChoice() {
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    computerChoice = randomChoice;
-    computerChoiceDisplay.innerHTML = `Computer choice: ` + randomChoice;
-}
 
-const handleClick = (e) => {
-    userChoice = e.target.id;
-    userChoiceDisplay.innerHTML =`User choice: ` + userChoice;
-    generateComputerChoice();
-    getResult();
-}
+document.getElementById('rock').addEventListener('click', () =>{
+    playerChoice = 0;
+    document.getElementById('playerChoice').src = "Resources/rock.png";
+})
 
+document.getElementById('paper').addEventListener('click', () =>{
+    playerChoice = 0;
+    document.getElementById('playerChoice').src = "Resources/paper.png";
+})
 
-    for (let i = 0; i < choices.length; i++) {
-        const button = document.createElement(`button`);
-        button.id = choices[i];
-        button.innerHTML = choices[i]
-        button.addEventListener(`click`, handleClick);
-        gameGrid.appendChild(button)
+document.getElementById('scissor').addEventListener('click', () =>{
+    playerChoice = 0;
+    document.getElementById('playerChoice').src = "Resources/scissor.jpg";
+})
+
+document.getElementById('lizard').addEventListener('click', () =>{
+    playerChoice = 0;
+    document.getElementById('playerChoice').src = "Resources/lizard.png";
+})
+
+document.getElementById('spock').addEventListener('click', () =>{
+    playerChoice = 0;
+    document.getElementById('playerChoice').src = "Resources/spock.jpg";
+})
+
+document.getElementById('generate').addEventListener('click', () => {
+    computerChoice = Math.round(Math.random()*4);
+    console.log(computerChoice);
+
+    if (computerChoice === 0){
+        document.getElementById('computerChoice').src = "Resources/rock.png";
+    } else if (computerChoice === 1){
+        document.getElementById('computerChoice').src = "Resources/paper.png";
+    } else if (computerChoice === 2){
+        document.getElementById('computerChoice').src = "Resources/scissor.jpg";
+    }else if (computerChoice === 3){
+        document.getElementById('computerChoice').src = "Resources/lizard.png";
+    }else {
+        document.getElementById('computerChoice').src = "Resources/spock.jpg";
     }
+});
 
-    const getResult = () => {
-        switch (userChoice + computerChoice) {
-            case `scissorpaper`:
-            case `rockscissors`:
-            case `paperrock`:
-                resultDisplay.innerHTML = "YOU WIN!";
-                break
-            case `paperscissors`:
-            case `scissorsrock`:
-            case `rockpaper`:
-                resultDisplay.innerHTML = "YOU LOSE!";
-                break
-            case `paperpaper`:
-            case `scissorsscissors`:
-            case `rockrock`:
-                resultDisplay.innerHTML = "IT'S A DRAW!";
-                break
-        }
-
+document.getElementById('win').addEventListener('click', () => {
+    if (playerChoice === computerChoice){
+        document.getElementById('result').innerHTML = "DRAW!"
+    }else if(computerChoice === 0 && playerChoice === 1 || computerChoice === 0 && playerChoice === 4){
+        document.getElementById('result').innerHTML = "PLAYER WINS!";
     }
+    else if(computerChoice === 1 && playerChoice === 2 || computerChoice === 1 && playerChoice ===3 ){
+        document.getElementById('result').innerHTML = "PLAYER WINS!";
+    }
+    else if(computerChoice === 2 && playerChoice === 0 || computerChoice === 2 && playerChoice === 4){
+        document.getElementById('result').innerHTML = "PLAYER WINS!";
+    }
+    else if(computerChoice === 3 && playerChoice === 0 || computerChoice === 3 && playerChoice ===2){
+        document.getElementById('result').innerHTML = "PLAYER WINS!";
+    }
+    else if (computerChoice === 4 && playerChoice === 1 || computerChoice === 4 && playerChoice === 3){
+        document.getElementById('result').innerHTML = "PLAYER WINS!";
+    }
+    else{
+        document.getElementById('result').innerHTML = "COMPUTER WINS!";
+    }
+});
